9aacd9f25f1e533a1761e281cd324b0e0c37062f
Fatima Zulfiqar <zulfiqarfatima022@gmail.com>
2023-10-22 01:50:26 +0500

    lab 3

--- calculator.c
+++ calculator.c
@@ -38,12 +38,25 @@ int main(int argc, char **argv) {
     printf("%f", result);
   } else if(choice == 3) {
     //TODO: handle this case (multiplication)
+    result = a * b;
+    printf("%f", result);
   } else if(choice == 4) {
     //TODO: handle this case (division)
+    result = a / b
+    printf("%f", result);
   } else if(choice == 5) {
     //TODO: handle this case (minimum)
+    result = mi(a,b);
+    printf("%f", result);
   } else if(choice == 6) {
     //TODO: handle this case (log_a(b))
+    if (a>0 && b>0){
+      result = log(a) / log(b);
+      printf("%f", result);
+    }
+    else {
+      printf("Error,you entered a negative value/n")
+    }
   } else {
     printf("Please input a valid operator next time");
   }
